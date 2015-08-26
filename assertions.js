0e5c92e035934e7880e5887cc3568ecd04fb0a8d
Stephen Young <stephenjayyoung@gmail.com>
2015-08-26 10:46:22 -0700

    first commit - added examples

---
+++ assertions.js
@@ -0,0 +1,19 @@
+// assert.equal(actual, expected[, message])
+
+// var arr = [1,2,3],
+//     lastElem = arr.pop();
+
+// assert.equal(lastElem, 3, "expected " + lastElem + " to equal 3, but it didn't!" );
+// var test = require('unit.js');
+var assert = require('assert');
+
+var arr = [1,2,3],
+    lastElem = arr.pop();
+
+assert.equal(lastElem, 2, "expected " + lastElem + " to equal 3, but it didn't!");
+
+
+
+// map
+// forEach
+// join
